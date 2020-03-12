4675cf9a007ad8690a29505b4ea57f72eb4227fe
Not Mike <notmike@notmike.co.uk>
2020-03-12 15:14:45 +0000

    Only compile DEBUG output for DEBUG builds

--- Linux/Debug.h
+++ Linux/Debug.h
@@ -5,4 +5,8 @@ extern BYTE DebugEnabled;
 void Debug(int DebugLevel, String File, unsigned long Line, String Format, ...);
 
 #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#ifdef _DEBUG
 #define DEBUG(DebugLevel, ...) Debug(DebugLevel, __FILENAME__, __LINE__, __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
